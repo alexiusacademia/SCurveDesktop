e7aca2f8453d65416caaa6a10b6a65c8993fd9ea
ALEXIUS ACADEMIA <alexius.academia@gmail.com>
2018-08-17 13:43:48 +0800

    Add serial to classes

--- src/scurvedesktop/views/ColorSettings.java
+++ src/scurvedesktop/views/ColorSettings.java
@@ -17,8 +17,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.util.prefs.Preferences;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
@@ -28,6 +26,7 @@ import java.awt.Font;
 import java.awt.Color;
 import javax.swing.border.BevelBorder;
 
+@SuppressWarnings("serial")
 public class ColorSettings extends JDialog implements ActionListener {
 
 	private final JPanel contentPanel = new JPanel();
